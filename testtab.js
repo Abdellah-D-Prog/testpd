3f7f82a49140b11533e5c1937e59532d9597944f
Abdellah-D-Prog <djezzar.abdellah@hotmail.com>
2024-11-08 13:47:09 +0100

    FAb c'est vraiment un putain de pd

---
+++ testtab.js
@@ -0,0 +1,21 @@
+//Tableau bite
+let bite=["a","b","c","d","e"];
+
+function bjr(bitef)
+{
+   for (let i = 0; i < bitef.length; i++) {
+    console.log(bitef[i]);
+    
+   }
+   return bitef;
+}
+
+bjr(bite);
+
+
+function fablegros(salope,grossepute)
+{
+    result=salope+grossepute;
+    console.log("t'es toujours gros pd "+ result);
+    return result;
+}
